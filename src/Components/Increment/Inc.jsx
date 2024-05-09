dcf7f6c5ccf612e07d3e6cada70bf0204b32e9a6
zafarbek2005 <153996622+zafarbek2005@users.noreply.github.com>
2024-05-09 15:36:27 +0500

    2

--- src/Components/Increment/Inc.jsx
+++ src/Components/Increment/Inc.jsx
@@ -4,10 +4,11 @@ import { useDispatch } from 'react-redux'
 
 
 const Inc = () => {
-
     let dispatch = useDispatch()
   return (
    <>
+   <input type="number"onSubmit={()=>dispatch(increment())} />
+   <button  onClick={()=>dispatch(increment())}>sumbit</button>
    <button  onClick={()=>dispatch(increment())}>increment</button>
    
    </>
